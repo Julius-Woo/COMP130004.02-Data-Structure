6a75aa67905c4bd6695db0424c1c981bd015360e
Julius <286924032@qq.com>
2023-09-06 11:54:52 +0800

    add comment

--- lab1/insertionSort.py
+++ lab1/insertionSort.py
@@ -1,10 +1,10 @@
 def insertionSort(array):
     n = len(array)
     for j in range(1,n):
-        key = array[j]
-        i = j - 1
-        while i >= 0 and key < array[i]:
-            array[i+1] = array[i]
+        key = array[j] # key is the value we are trying to insert
+        i = j - 1 # i is the index of the value to the left of key
+        while i >= 0 and key < array[i]: # while i is not out of bounds and key is less than the value to the left of it
+            array[i+1] = array[i] # move the value to the left of key one index to the right
             i = i - 1
-        array[i+1] = key
+        array[i+1] = key # insert key into the correct position
     return array
--- lab1/mergeSort.py
+++ lab1/mergeSort.py
@@ -1,4 +1,4 @@
-def merge(left, right):
+def merge(left, right): # merge two sorted arrays
     result = []
     i = 0
     j = 0
@@ -6,13 +6,13 @@ def merge(left, right):
     rightlen = len(right)
     while i < leftlen and j < rightlen:
         if left[i] < right[j]:
-            result.append(left[i])
+            result.append(left[i]) # append the smaller value
             i += 1
         else:
             result.append(right[j])
             j +=1
-    result += left[i:]
-    result += right[j:]
+    result += left[i:] # append the rest of the values
+    result += right[j:] # append the rest of the values
     return result
 
 def mergeSort(array):
@@ -20,9 +20,9 @@ def mergeSort(array):
     if n == 1:
         return array
     else:
-        mid = int(n/2)
-        left = array[:mid]
-        right = array[mid:]
-        left = mergeSort(left)
-        right = mergeSort(right)
-        return merge(left, right)
+        mid = int(n/2) # split the array in half
+        left = array[:mid] # left half
+        right = array[mid:] # right half
+        left = mergeSort(left) # recursively sort the left half
+        right = mergeSort(right) # recursively sort the right half
+        return merge(left, right) # merge the two sorted halves
